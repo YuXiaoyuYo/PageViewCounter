65f84f0a6865f4a1b5aeeb8d1f42edaaeaa9a8e5
YuXiaoyuYo <3039139525@qq.com>
2024-10-15 01:21:09 +0800

    Update with CORS

--- src/worker.js
+++ src/worker.js
@@ -1,20 +1,23 @@
 export default {
 	async fetch(request, env) {
-	  const url = new URL(request.url);
-	  const page = url.searchParams.get('page');
-	  if (url.pathname === '/api/count' && request.method === 'POST') {
-		  return await incrementCount(page, env);
-	  }
-	  return new Response('Invalid Request', { status: 400 });
+		if (new URL(request.url).origin !== 'https://yu.nm.cn') {
+			return new Response('Forbidden', { status: 403 });
+		}
+
+		const page = new URL(request.url).searchParams.get('page');
+		if (url.pathname === '/api/count' && request.method === 'POST') {
+			return await incrementCount(page, env);
+		} else {
+			return new Response('Method Not Allowed', { status: 405 });
+		}
 	}
-  }
-  
-  async function incrementCount(page, env) {
+}
+
+async function incrementCount(page, env) {
 	const query = `INSERT INTO views (page, count) VALUES (?, 1)
 				   ON CONFLICT(page) DO UPDATE SET count = count + 1
 				   RETURNING count`;
 	const result = await env['DB'].prepare(query).bind(page).first();
 	const count = result.count;
 	return new Response(count.toString(), { status: 200 });
-  }
-  
+}
