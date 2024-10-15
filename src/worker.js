4395ed680491dfa13a1cb3edb80570e992b5a11d
YuXiaoyuYo <3039139525@qq.com>
2024-10-15 14:44:41 +0800

    删除分页数量统计

--- src/worker.js
+++ src/worker.js
@@ -6,25 +6,13 @@ export default {
 			return new Response('Forbidden', { status: 403 });
 		}
 
-		const { action, page } = await request.json();
-		if (action === 'increment') {
-			return await incrementCount(page, env);
-		} else if (action === 'total') {
+		const { action } = await request.json();
+		if (action === 'total') {
 			return await getTotalCount(env);
 		} else {
 			return new Response('Method Not Allowed', { status: 405 });
 		}
 	}
-}
-
-async function incrementCount(page, env) {
-	// 更新页面浏览量
-	const updatePageQuery = `INSERT INTO views (page, count) VALUES (?, 1)
-							 ON CONFLICT(page) DO UPDATE SET count = count + 1
-							 RETURNING count`;
-	const pageResult = await env['DB'].prepare(updatePageQuery).bind(page).first();
-	const pageCount = pageResult.count;
-	return new Response(pageCount.toString(), { status: 200 });
 }
 
 async function getTotalCount(env) {
