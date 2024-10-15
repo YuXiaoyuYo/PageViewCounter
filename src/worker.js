5e636fce67ac348a0fd0a954fcb5b7bd2bfbd353
YuXiaoyuYo <3039139525@qq.com>
2024-10-15 12:09:55 +0800

    添加全站浏览计数

--- src/worker.js
+++ src/worker.js
@@ -1,16 +1,18 @@
 export default {
 	async fetch(request, env) {
-
 		const url = new URL(request.url);
 
 		if (url.origin !== 'https://yu.nm.cn') {
 			return new Response('Forbidden', { status: 403 });
 		}
 
-		const { pathname } = url;
-		if (pathname === '/api/count' && request.method === 'POST') {
-			const { page } = await request.json();
-			return await incrementCount(page, env);
+		if (request.method === 'POST') {
+			const { action, page } = await request.json();
+			if (action === 'increment') {
+				return await incrementCount(page, env);
+			} else if (action === 'total') {
+				return await getTotalCount(env);
+			}
 		} else {
 			return new Response('Method Not Allowed', { status: 405 });
 		}
@@ -24,4 +26,11 @@ async function incrementCount(page, env) {
 	const result = await env['DB'].prepare(query).bind(page).first();
 	const count = result.count;
 	return new Response(count.toString(), { status: 200 });
+}
+
+async function getTotalCount(env) {
+	const query = `SELECT SUM(count) as total FROM views`;
+	const result = await env['DB'].prepare(query).first();
+	const total = result.total;
+	return new Response(total ? total.toString() : '0', { status: 200 });
 }
